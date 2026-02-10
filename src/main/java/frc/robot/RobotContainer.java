e5ee53554eac0c7291390b5aa3555d3e50a0d659
jonwoodard <jonwoodard@users.noreply.github.com>
2026-02-10 07:23:15 -0500

    no longer public class

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -74,7 +74,7 @@ public class RobotContainer
 
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
-    public RobotContainer() 
+    RobotContainer() 
     {
         // Instantiate ONLY the components selected above
         if(useFullRobot || useExampleSubsystem)
