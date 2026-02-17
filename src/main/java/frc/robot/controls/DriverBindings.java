7b7ece8a7a471ec7cb6f2efb0420679266cc7a08
JD5757 <Jacksondinges88@gmail.com>
2026-02-16 19:48:19 -0500

    JD Added bindings

--- src/main/java/frc/robot/controls/DriverBindings.java
+++ src/main/java/frc/robot/controls/DriverBindings.java
@@ -7,10 +7,17 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.RobotContainer;
+import frc.robot.commands.GeneralCommands;
+import frc.robot.sensors.Camera;
+import frc.robot.subsystems.Agitator;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.Flywheel;
 import frc.robot.subsystems.LEDs;
+import frc.robot.subsystems.Pivot;
 import frc.robot.subsystems.PoseEstimator;
+import frc.robot.subsystems.Roller;
+import frc.robot.subsystems.Shroud;
 
 public final class DriverBindings
 {
@@ -28,16 +35,24 @@ public final class DriverBindings
     // Put all class variables and instance variables here
     private static CommandXboxController controller;
 
-    private static Drivetrain drivetrain;
     private static Flywheel flywheel;
+    private static Agitator agitator;
+    private static Roller roller;
+    private static Pivot pivot;
+    private static Shroud shroud;
+    private static Drivetrain drivetrain;
     private static LEDs leds;
+
+    private static Camera camera;
     private static PoseEstimator poseEstimator;
 
+    private static final double CRAWL_SPEED = 0.225;
+    private static final double WALK_SPEED = 0.675;
+    private static final double RUN_SPEED = 1.0;
     private static DoubleSupplier leftYAxis;
     private static DoubleSupplier leftXAxis;
     private static DoubleSupplier rightXAxis;
     private static DoubleSupplier scaleFactorSupplier;
-
     private static double scaleFactor = 0.5;
 
     // *** CLASS CONSTRUCTORS ***
@@ -46,25 +61,46 @@ public final class DriverBindings
     /**
      * Creates button bindings for the operator
      * @param robotContainer
-     * @author Mukul Kedia
+     * @author Jackson D.
      */
 
     public static void createBindings(RobotContainer robotContainer)
     {
-        System.out.println("Creating Operator Bindings: " + fullClassName);
+        System.out.println("Creating Driver Bindings: " + fullClassName);
 
         controller = robotContainer.getDriverController();
+
         drivetrain = robotContainer.getDrivetrain();
         flywheel = robotContainer.getFlywheel();
+        agitator = robotContainer.getAgitator();
+        pivot = robotContainer.getPivot();
+        shroud = robotContainer.getShroud();
         leds = robotContainer.getLEDs();
+
+        camera = robotContainer.getCamera();
         poseEstimator = robotContainer.getPoseEstimator();
 
         if(controller != null)
         {
             configSuppliers();
             configDefaultCommands();
-            configStartButton();
+
             configAButton();
+            configBButton();
+            configXButton();
+            configYButton();
+            configLeftBumper();
+            configRightBumper();
+            configBackButton();
+            configStartButton();
+            configLeftTrigger();
+            configRightTrigger();
+            configLeftStick();
+            configRightStick();
+            configDpadUp();
+            configDpadDown(); 
+            configDpadLeft();
+            configDpadRight();
         }
     }
 
@@ -85,17 +121,94 @@ public final class DriverBindings
         System.out.println("Config Default Commands Driver Controller");
     }
 
-    private static void configStartButton()
-    {
-        Trigger startButton = controller.start();
-        startButton
-            .onTrue(Commands.runOnce(() -> drivetrain.resetForFieldCentric(), drivetrain));
-    }
-
     private static void configAButton()
     {
         Trigger aButton = controller.a();
         aButton
             .whileTrue(drivetrain.angleLockDriveCommand(leftYAxis, leftXAxis, scaleFactorSupplier, () -> (poseEstimator.getAngleToTarget(poseEstimator.getEstimatedPose(), poseEstimator.getAllianceHubPose()).getAsDouble())));
     }
+
+    private static void configBButton()
+    {
+        Trigger bButton = controller.b();
+    }
+
+    private static void configXButton()
+    {
+        Trigger xButton = controller.x();
+    }
+
+    private static void configYButton()
+    {
+        Trigger yButton = controller.y();
+    }
+
+    private static void configLeftBumper()
+    {
+        Trigger leftBumper = controller.leftBumper();
+        leftBumper
+            .onTrue(Commands.runOnce(() -> scaleFactor = (scaleFactor > (CRAWL_SPEED + WALK_SPEED) / 2.0) ? CRAWL_SPEED : WALK_SPEED));
+    }
+
+    private static void configRightBumper()
+    {
+        Trigger rightBumper = controller.rightBumper();
+    }
+
+    private static void configBackButton()
+    {
+        Trigger backButton = controller.back();
+    }
+
+    private static void configStartButton()
+    {
+        Trigger startButton = controller.start();
+        startButton
+        .onTrue(Commands.runOnce(() -> drivetrain.resetForFieldCentric(), drivetrain));
+    }
+
+    private static void configLeftTrigger()
+    {
+        Trigger leftTrigger = controller.leftTrigger();
+        leftTrigger
+            .onTrue(Commands.runOnce(() -> scaleFactor = (scaleFactor > (WALK_SPEED + RUN_SPEED) / 2.0) ? WALK_SPEED : RUN_SPEED));
+    }
+
+    private static void configRightTrigger()
+    {
+        Trigger rightTrigger = controller.rightTrigger();
+    }
+
+    private static void configLeftStick()
+    {
+        Trigger leftStick = controller.leftStick();
+    }
+
+    private static void configRightStick()
+    {
+        Trigger rightStick = controller.rightStick();
+    }
+
+    private static void configDpadUp()
+    {
+        Trigger dpadUp = controller.povUp();
+    }
+
+    private static void configDpadDown()
+    {
+        Trigger dpadDown = controller.povDown();
+        dpadDown
+            .onTrue(GeneralCommands.stopAllCommand());
+    }
+
+    private static void configDpadLeft()
+    {
+        Trigger dpadLeft = controller.povLeft();
+    }
+
+    private static void configDpadRight()
+    {
+        Trigger dpadRight = controller.povRight();
+    }
+
 }
--- src/main/java/frc/robot/controls/OperatorBindings.java
+++ src/main/java/frc/robot/controls/OperatorBindings.java
@@ -1,9 +1,24 @@
 package frc.robot.controls;
 
 import java.lang.invoke.MethodHandles;
+import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.RobotContainer;
+import frc.robot.commands.GeneralCommands;
+import frc.robot.commands.IntakingCommands;
+import frc.robot.commands.ScoringCommands;
+import frc.robot.sensors.Camera;
+import frc.robot.subsystems.Agitator;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Flywheel;
+import frc.robot.subsystems.LEDs;
+import frc.robot.subsystems.Pivot;
+import frc.robot.subsystems.PoseEstimator;
+import frc.robot.subsystems.Roller;
+import frc.robot.subsystems.Shroud;
 
 public final class OperatorBindings 
 {
@@ -20,7 +35,22 @@ public final class OperatorBindings
     // *** CLASS VARIABLES & INSTANCE VARIABLES ***
     // Put all class variables and instance variables here
     private static CommandXboxController controller;
-    // Other class variables to store subsystems from robot container.
+
+    private static Flywheel flywheel;
+    private static Agitator agitator;
+    private static Roller roller;
+    private static Pivot pivot;
+    private static Shroud shroud;
+    private static Drivetrain drivetrain;
+    private static LEDs leds;
+
+    private static Camera camera;
+    private static PoseEstimator poseEstimator;
+
+    private static DoubleSupplier leftYAxis;
+    private static DoubleSupplier leftXAxis;
+    private static DoubleSupplier rightXAxis;
+    private static DoubleSupplier rightYAxis;
 
     // *** CLASS CONSTRUCTORS ***
     // Put all class constructors here
@@ -28,18 +58,149 @@ public final class OperatorBindings
     /**
      * Creates button bindings for the operator
      * @param robotContainer
-     * @author Mukul Kedia
+     * @author Jackson D.
      */
 
     public static void createBindings(RobotContainer robotContainer)
     {
         System.out.println("Creating Operator Bindings: " + fullClassName);
 
-        // [thing] = robotContainer.get[thing]s here.
+        controller = robotContainer.getOperatorController();
+
+        drivetrain = robotContainer.getDrivetrain();
+        flywheel = robotContainer.getFlywheel();
+        agitator = robotContainer.getAgitator();
+        pivot = robotContainer.getPivot();
+        shroud = robotContainer.getShroud();
+        leds = robotContainer.getLEDs();
+
+        camera = robotContainer.getCamera();
+        poseEstimator = robotContainer.getPoseEstimator();
 
         if(controller != null)
         {
-            // Config[Method]s here.
+            configSuppliers();
+            configDefaultCommands();
+
+            configAButton();
+            configBButton();
+            configXButton();
+            configYButton();
+            configLeftBumper();
+            configRightBumper();
+            configBackButton();
+            configStartButton();
+            configLeftTrigger();
+            configRightTrigger();
+            configLeftStick();
+            configRightStick();
+            configDpadUp();
+            configDpadDown(); 
+            configDpadLeft();
+            configDpadRight();
         }
+    }
+
+    private static void configSuppliers()
+    {
+        leftYAxis = () -> -controller.getRawAxis(1);
+        leftXAxis = () -> -controller.getRawAxis(0);
+        rightXAxis = () -> -controller.getRawAxis(4);
+        rightYAxis = () -> -controller.getRawAxis(5);
+    }
+
+    private static void configDefaultCommands()
+    {}
+
+    private static void configAButton()
+    {
+        Trigger aButton = controller.a();
+        aButton
+            .onTrue(ScoringCommands.stationaryScoreCommand());
+    }
+
+    private static void configBButton()
+    {
+        Trigger bButton = controller.b();
+        bButton
+            .onTrue(ScoringCommands.stopScoringCommand());
+    }
+
+    private static void configXButton()
+    {
+        Trigger xButton = controller.x();
+        xButton
+            .onTrue(IntakingCommands.simpleIntakeCommand());
+    }
+
+    private static void configYButton()
+    {
+        Trigger yButton = controller.y();
+        yButton
+            .onTrue(IntakingCommands.stopIntakingCommand());
+    }
+
+    private static void configLeftBumper()
+    {
+        Trigger leftBumper = controller.leftBumper();
+        leftBumper
+            .onTrue(ScoringCommands.simpleScoreCommand());
+    }
+
+    private static void configRightBumper()
+    {
+        Trigger rightBumper = controller.rightBumper();
+    }
+
+    private static void configBackButton()
+    {
+        Trigger backButton = controller.back();
+    }
+
+    private static void configStartButton()
+    {
+        Trigger startButton = controller.start();
+    }
+
+    private static void configLeftTrigger()
+    {
+        Trigger leftTrigger = controller.leftTrigger();
+    }
+
+    private static void configRightTrigger()
+    {
+        Trigger rightTrigger = controller.rightTrigger();
+    }
+
+    private static void configLeftStick()
+    {
+        Trigger leftStick = controller.leftStick();
+    }
+
+    private static void configRightStick()
+    {
+        Trigger rightStick = controller.rightStick();
+    }
+
+    private static void configDpadUp()
+    {
+        Trigger dpadUp = controller.povUp();
+    }
+
+    private static void configDpadDown()
+    {
+        Trigger dpadDown = controller.povDown();
+        dpadDown
+            .onTrue(GeneralCommands.stopAllCommand());
+    }
+
+    private static void configDpadLeft()
+    {
+        Trigger dpadLeft = controller.povLeft();
+    }
+
+    private static void configDpadRight()
+    {
+        Trigger dpadRight = controller.povRight();
     }
 }
